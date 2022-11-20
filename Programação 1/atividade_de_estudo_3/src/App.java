f554e4b7cd0c3dea49a9c0b7bb5fa0637421fdbb
Murilo Juvêncio <dmurild-juvencio@hotmail.com.br>
2022-11-19 22:28:14 -0300

    Melhoria na leitura geral

--- Programação 1/atividade_de_estudo_3/src/App.java
+++ Programação 1/atividade_de_estudo_3/src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static final float valor_dependente = 134.98F;
-
+    public static final float percentual_dependente = 0.03f;
     public static void main(String[] args) throws Exception {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Informe o salário:");
@@ -10,7 +10,8 @@ public class App {
             System.out.println("Informe a quantidade de dependentes");
             int dependentes = scanner.nextInt();
 
-            System.out.printf("Resultado o salário %.2f \n", salario);
+            System.out.println("************* Resultados *************");
+            System.out.printf("Salário %.2f \n", salario);
             System.out.printf("Quantidade de dependentes: %d \n", dependentes);
             System.out.printf("Total do salário família: %.2f \n", adicionalDependetes(dependentes, salario));
             System.out.printf("Salário final: %.2f \n", salario + adicionalDependetes(dependentes, salario));
@@ -18,7 +19,7 @@ public class App {
     }
 
     public static float adicionalDependetes(int dependentes, float salario) {
-        float valor_familia = (dependentes * valor_dependente) + (salario * (dependentes * 3) % 100);
+        float valor_familia = (dependentes * valor_dependente) + (salario * (dependentes * percentual_dependente));
 
         return valor_familia;
     }
